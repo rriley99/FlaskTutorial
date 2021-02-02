75ed0cb293c615f45114b6e2f3194de0e16b5b42
Robert Riley <rriley99@gmail.com>
2021-02-02 13:25:00 -0500

    Added up to part 5

--- README.md
+++ README.md
@@ -6,6 +6,12 @@ Code from following Youtube's Corey Schafer's series on Python Flask Tutorial
 ## Part Two
 - [x] Template Inheritance 
 - [x] HTML and CSS to make it all look nice and neat. 
-
+## Part Three
+- [x] Forms
+- [x] User Inputs
+- [x] Validate Inputs
+## Part Four
+- [x] SQLite and SQLAlchemy ORM
+  
 ## Link to Source
 https://www.youtube.com/watch?v=MwZwr5Tvyxo&list=PL-osiE80TeTs4UjLw5MM6OjgkjFeUxCYH
--- flaskblog.py
+++ flaskblog.py
@@ -1,5 +1,34 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for, flash, redirect
+from forms import RegistrationForm, LoginForm
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+
 app = Flask(__name__)
+
+app.config['SECRET_KEY'] = 'd3e6c80a24cd9a80dfa1658b02d07f3a'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+db = SQLAlchemy(app)
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(20), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    image_file = db.Column(db.String(20), nullable=False, default='default.jpg')
+    password = db.Column(db.String(60), nullable=False)
+    posts = db.relationship('Post', backref='author', lazy=True)
+
+    def __repr__(self):
+        return f"User('{self.username}', '{self.email}', '{self.image_file}')"
+
+class Post(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    date_posted = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    content = db.Column(db.Text, nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+
+    def __repr__(self):
+        return f"Post('{self.title}','{self.date_posted}')"
 
 posts = [
     {
@@ -18,12 +47,41 @@ posts = [
 ]
 
 @app.route("/")
-def hello():
+def home():
     return render_template('home.html', posts=posts)
 
 @app.route("/about")
 def about():
     return render_template('about.html')
+
+@app.route("/register", methods=['GET', 'POST'])
+def register():
+    
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        flash(f'Account Created for {form.username.data}!', 'success')
+        return redirect(url_for('home'))
+
+    return render_template('register.html', title = 'Register', form=form)
+
+@app.route("/login", methods=['GET','POST'])
+def login():
+    
+    form = LoginForm()
+
+    if form.validate_on_submit():
+
+        if form.email.data == 'admin@blog.com' and form.password.data == 'password':
+            
+            flash(f'You have been logged in!', 'success')
+            
+            return redirect(url_for('home'))
+        
+        else:
+
+            flash('Login Unsuccessful. Please check username and password', 'danger') 
+
+    return render_template('login.html', title = 'Login', form=form)
 
 if __name__ == '__main__':
     app.run(debug=True)
